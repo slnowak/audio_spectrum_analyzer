0b5127e67f5430394c2c3f9979c84f252e5b8f76
novy154 <slnowak154@gmail.com>
2014-11-11 10:54:15 +0100

    Added methods to draw grid on canvas

---
+++ presenter/canvas_presenter.py
@@ -0,0 +1,9 @@
+__author__ = 'novy'
+
+
+class CanvasPresenter(object):
+    def __init__(self, canvas_view):
+        self.canvas_view = canvas_view
+
+    def read_data(self):
+        pass
--- view/canvas_panel.py
+++ view/canvas_panel.py
@@ -8,17 +8,21 @@ BORDER_WIDTH = 5
 RELIEF = RIDGE
 
 # todo: fix it somehow
-GRID_WIDTH = 800                  # Width of the grid
-GRID_HEIGHT = 400                  # Height of the grid
-X0L = 20                    # Left top X value of grid
-Y0T = 25                    # Left top Y value of grid
-CANVAS_WIDTH = GRID_WIDTH + 2 * X0L
-CANVAS_HEIGHT = GRID_HEIGHT + 80
+GRID_WIDTH = 800
+GRID_HEIGHT = 400
+LEFT_TOP_GRID_X_POSITION = 20
+LEFT_TOP_GRID_Y_POSITION = 20
+HORIZONTAL_CELLS = 8
+VERTICAL_CELLS = 10
+GRID_COLOR = "#808080"
+
+CANVAS_WIDTH = GRID_WIDTH + 2 * LEFT_TOP_GRID_X_POSITION
+CANVAS_HEIGHT = GRID_HEIGHT + 2 * LEFT_TOP_GRID_Y_POSITION
 CANVAS_BACKGROUND = "#000000"
 
 
 class CanvasPanel(Frame):
-    def __init__(self, master=None, cnf={}, **kw):
+    def __init__(self, master=None, cnf={}, presenter=None, **kw):
         Frame.__init__(self, master, cnf,
                        background=BACKGROUND,
                        borderwidth=BORDER_WIDTH,
@@ -26,9 +30,44 @@ class CanvasPanel(Frame):
 
         self.pack(side=TOP, expand=1, fill=X)
 
+        self.presenter = presenter
+
         self.canvas = self.__init_canvas()
+        self.draw_grid()
 
     def __init_canvas(self):
         canvas = Canvas(self, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, background=CANVAS_BACKGROUND)
         canvas.pack(side=TOP)
         return canvas
+
+    def draw_grid(self):
+        self.__draw_horizontal_lines()
+        self.__draw_vertical_lines()
+
+    def __draw_horizontal_lines(self):
+        top_left_x = LEFT_TOP_GRID_X_POSITION
+        top_right_x = CANVAS_WIDTH - LEFT_TOP_GRID_X_POSITION
+        cell_height = GRID_HEIGHT / HORIZONTAL_CELLS
+
+        lines = [
+            (top_left_x, y, top_right_x, y) for y in
+            [LEFT_TOP_GRID_Y_POSITION + cell_number * cell_height for cell_number in xrange(HORIZONTAL_CELLS + 1)]
+        ]
+
+        self.__draw_lines(lines)
+
+    def __draw_vertical_lines(self):
+        left_top_y = LEFT_TOP_GRID_Y_POSITION
+        left_bottom_y = CANVAS_HEIGHT - LEFT_TOP_GRID_Y_POSITION
+        cell_width = GRID_WIDTH / VERTICAL_CELLS
+
+        lines = [
+            (x, left_top_y, x, left_bottom_y) for x in
+            [LEFT_TOP_GRID_X_POSITION + cell_number * cell_width for cell_number in xrange(VERTICAL_CELLS + 1)]
+        ]
+
+        self.__draw_lines(lines)
+
+    def __draw_lines(self, lines):
+        for line in lines:
+            self.canvas.create_line(line, fill=GRID_COLOR)
